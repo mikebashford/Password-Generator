b03a1f6231e064c2e35a8ab154d0e19bda60d9af
Michael Bashford <mikebashford@gmail.com>
2021-11-15 19:19:41 -0600

    Moved code to generatePassword function given, returns correct values and writes into the box.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,65 +5,6 @@ var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "'","{", "}", "<", ";",":", "[","]",",",">","/","\\","?","~","`"];
 var numbers = ["1","2","3","4","5","6","7","8","9","0"];
 
-function promptUser()
-{ 
-    var passwordLength = window.prompt("Please choose between 8 and 128 characters.");
-    if(isNaN(passwordLength))
-    {
-      console.log("This is not a number");
-      promptUser();
-    }
-    else
-    {
-      passwordLength = parseInt(password);
-    }
-    var hasLower = window.confirm("Would you like to include lower case letters?");
-    var hasUpper = window.confirm("Would you like to include upper case letters?");
-    var hasSymbol = window.confirm("Would you like to include symbols?");
-    var hasNumber = window.confirm("Would you like to include numbers?");
-
-    var tempPass = "";
-
-    if(hasLower)
-    {
-      for(i = 0; i < letters.length; i++)
-      {
-        tempPass += letters[i];
-        tempPass = tempPass.toLowerCase();
-        console.log(tempPass);
-      }
-    }
-
-    if(hasUpper)
-    {
-      for(i = 0; i < letters.length; i++)
-      {
-        tempPass += letters[i];
-        console.log(tempPass);
-      }
-    }
-
-    if(hasSymbol)
-    {
-      for(i = 0; i < symbols.length; i++)
-      {
-        tempPass += symbols[i];
-        console.log(tempPass);
-      }
-    }
-
-    if(hasNumber)
-    {
-      for(i = 0; i < numbers.length; i++)
-      {
-        tempPass += numbers[i];
-        console.log(tempPass);
-      }
-    }
-
-    console.log(tempPass.length);
-}
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -77,7 +18,65 @@ function writePassword() {
 
 function generatePassword()
 {
-  promptUser();
+  var passwordLength = window.prompt("Please choose between 8 and 128 characters.");
+  if(isNaN(passwordLength))
+  {
+    console.log("This is not a number");
+    promptUser();
+  }
+  else
+  {
+    passwordLength = parseInt(passwordLength);
+  }
+
+  var hasLower = window.confirm("Would you like to include lower case letters?");
+  var hasUpper = window.confirm("Would you like to include upper case letters?");
+  var hasSymbol = window.confirm("Would you like to include symbols?");
+  var hasNumber = window.confirm("Would you like to include numbers?");
+
+  var tempPass = "";
+
+  if(hasLower)
+  {
+    for(i = 0; i < letters.length; i++)
+    {
+      tempPass += letters[i];
+      tempPass = tempPass.toLowerCase();
+    }
+  }
+
+  if(hasUpper)
+  {
+    for(i = 0; i < letters.length; i++)
+    {
+      tempPass += letters[i];
+    }
+  }
+
+  if(hasSymbol)
+  {
+    for(i = 0; i < symbols.length; i++)
+    {
+      tempPass += symbols[i];
+    }
+  }
+
+  if(hasNumber)
+  {
+    for(i = 0; i < numbers.length; i++)
+    {
+      tempPass += numbers[i];
+    }
+  }
+
+  var password = "";
+
+  for(i=0; i < passwordLength; i++)
+  {
+    var index = (Math.floor(Math.random() * tempPass.length));
+    password += tempPass[index];
+  }
+  return password;
 }
 
 // Add event listener to generate button
